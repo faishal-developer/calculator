fde2df5a06d504231f178915619406abc81f5f0f
faishal-developer <foysal.professional@gmail.com>
2021-08-10 10:15:35 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+let btn = document.getElementsByTagName('button')
+let input = document.getElementsByTagName('input')
+let inputArr = [...input]
+let btnArr = [...btn]
+
+let inputValue=''
+btnArr.forEach((v,i)=>{
+  v.addEventListener('click',function(e){
+    inputValue += e.target.value;
+    if(v.value === 'clr') inputValue='' 
+    if(v.value === '=') inputValue = calculate(inputValue) 
+    input[0].value=inputValue;
+  })
+})
+
+
+function calculate(str) {
+    let strToNumber=str.match(/\d+(\.)?(\d+)?/g)
+    if(/\//.test(str)) return Number(strToNumber[0]) / Number(strToNumber[1])
+    if(/\*/.test(str)) return Number(strToNumber[0]) * Number(strToNumber[1])
+    if(/\-/.test(str)) return Number(strToNumber[0]) - Number(strToNumber[1])
+    if(/\+/.test(str)) return Number(strToNumber[0]) + Number(strToNumber[1])
+}
